2cdea07ca2b9e1a76ac762975e7110577bff6950
bbexpress <bbexpress@gmail.com>
2021-12-02 12:04:30 -0600

    more layout changes

--- src/Components/Budget.js
+++ src/Components/Budget.js
@@ -5,13 +5,14 @@ export default class Budget extends Component {
   render() {
     return (
       <div>
-          <div className="container">
+          <div className="container" style = {{backgroundColor: "lightgrey"}}>
               <h2>Budget: $2000</h2>
+              <button type="button" className="btn btn-primary">Edit</button>
         </div> 
-          <div className="container">
+          <div className="container" style = {{backgroundColor: "lightgreen"}}>
               <h2>Remaining: $500</h2>
           </div> 
-          <div className="container">
+          <div className="container" style = {{backgroundColor: "lightblue"}}>
               <h2>Expenses: $1500</h2>
           </div> 
       </div>
